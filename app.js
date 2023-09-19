693f6c37282c8778c304ae4ae3ad43e70972cb06
Joel <joepotential@gmail.com>
2023-09-19 09:09:03 +0100

    accordion done

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const menu = document.querySelector('.menu')
 const menuBtn = document.querySelector('.menu-btn')
 const closeBtn = document.querySelector('.close-btn')
+const bodys = document.querySelectorAll('.question')
 
 
 //TOGGLE MENU BUTTON
@@ -11,4 +12,20 @@ menuBtn.addEventListener('click', function() {
 
 closeBtn.addEventListener('click', function() {
     menu.classList.remove('open')
+})
+
+
+//ACCORDION TOGGLE
+bodys.forEach(function(body) {
+    const btn = body.querySelector('.question-head')
+
+    btn.addEventListener('click', function() {
+        bodys.forEach(function(item) {
+            if (item !== body) {
+                item.classList.remove('open')
+            }
+        })
+
+        body.classList.toggle('open')
+    })
 })
