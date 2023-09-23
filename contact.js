11e5959004cbca439a0024d8c2bbc30440ce52e1
Joel <joepotential@gmail.com>
2023-09-23 07:05:21 +0100

    contact mobile page done

---
+++ contact.js
@@ -0,0 +1,8 @@
+//VARIABLES
+const backBtn = document.querySelector('.back-btn')
+console.log(backBtn);
+
+//BACK TO HOMEPAGE LOGIC
+backBtn.addEventListener('click', function() {
+    window.location.href = 'index.html'
+});
