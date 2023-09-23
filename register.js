89d4d778cf47fcb973a795cbb80413d748608c0d
Joel <joepotential@gmail.com>
2023-09-23 22:51:31 +0100

    overlay added

--- register.js
+++ register.js
@@ -1,8 +1,19 @@
 //DECLARATIONS
 const registerBtn = document.querySelector('.register-btn1')
-
+const backBtn = document.querySelector('.back-btn')
+const submitBtn = document.querySelector('.submit-btn')
+const overlay = document.querySelector('.overlay')
 
 //NAVIGATE TO REGISTER PAGE
 registerBtn.addEventListener('click', function() {
-  window.location.href = 'register.html'
+    window.location.href = 'register.html'
+})
+
+submitBtn.addEventListener('submit', function(e)  {
+    e.preventDefault
+    overlay.classList.add('open')
+})
+
+backBtn.addEventListener('click', function() {
+    overlay.classList.remove('open')
 })
